cc0ad8402b7ea8a4df117574ce5e5a1a75d2ccea
CrazyChair69 <Christianschwanse@gmx.de>
2024-05-25 16:25:26 +0200

    Bugfix: Unresponsive Header + Code-Refactoring

--- components/header/header.js
+++ components/header/header.js
@@ -9,31 +9,25 @@ class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
     <header id="page-header">
-        <nav>
-            <h2 class="visually-hidden">Navigationsmenü</h2>
+        <h2 class="visually-hidden">Navigationsmenü</h2>
 
-            <div id="page-header-flex">
-                <ul id="page-header-left">
-                    <li>
-                        <a href="index.html" id="header-logo">Christian Schwanse
-                        </a>
-                    </li>
-                </ul>
+        <a href="index.html" id="header-logo">Christian Schwanse</a>
 
-                <ul id="page-header-right">
-                    <li class="header-button"><a href="about-me.html">Über mich</a></li>
-                    <li class="header-button"><a href="my-projects.html">Projekte</a></li>
-                    <li class="header-button"><a href="contact.html">Kontakt</a></li>
-                    <li>
-                        <div id="theme-switch-wrapper">
-                            <label id="theme-switch" for="theme-checkbox">
-                                <input type="checkbox" name="theme-checkbox" id="theme-checkbox" />
-                                <div id="theme-slider"></div>
-                            </label>
-                        </div>
-                    </li>
-                </ul>
-            </div>
+
+        <nav id="page-header-nav">
+            <ul id="page-header-nav-list">
+                <li class="header-button"><a href="about-me.html">Über mich</a></li>
+                <li class="header-button"><a href="my-projects.html">Projekte</a></li>
+                <li class="header-button"><a href="contact.html">Kontakt</a></li>
+                <li>
+                    <div id="theme-switch-wrapper">
+                        <label id="theme-switch" for="theme-checkbox">
+                            <input type="checkbox" name="theme-checkbox" id="theme-checkbox" />
+                            <div id="theme-slider"></div>
+                        </label>
+                    </div>
+                </li>
+            </ul>
         </nav>
     </header>
     `;
