67049cee24f1ad589c55d62d95f30153a115ddb6
CrazyChair69 <Christianschwanse@gmx.de>
2024-07-23 19:08:00 +0200

    Add Redirect to correct index.html

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/*
+    This index.html serves as a placeholder. 
+    Is only used for a redirect on the server side. 
+
+    If I select the directory on Strato (domain hoster) where the index.html is located (directory: /en/), 
+    then the paths of the CSS and JS files are no longer correct. 
+    Loading the files therefore fails.
+
+    The desired index.html is located in the /de/ directory, 
+    as this is a multilingual site and the language of the individual pages +
+    is determined on the basis of the directories.
+
+    Example:
+    All German pages are in the /de/ directory.
+    All English pages are in the /en/ directory.
+*/
+header("Location: /de/index.html", true, 301);
+die();
