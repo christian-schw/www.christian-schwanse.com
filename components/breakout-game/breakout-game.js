f0724653deda62dd73967f5c5ed9db99c857097c
CrazyChair69 <Christianschwanse@gmx.de>
2024-08-12 12:04:52 +0200

    Add Animation Ball + Button-Tag Style

--- components/breakout-game/breakout-game.js
+++ components/breakout-game/breakout-game.js
@@ -1,1 +1,55 @@
-// Implement if needed
+window.onload = init();
+
+function init() {
+
+    // ========= Breakout Game =========
+    startBreakoutGame();
+}
+
+
+
+var ballX;
+var ballY;
+var ballDX = 2;
+var ballDY = -2;
+var breakoutCanvas;
+var breakoutCtx;
+
+
+function startBreakoutGame() {
+    breakoutCanvas = document.querySelector('#breakout-canvas');
+
+    // It's possible that some pages do not have the Breakout-Game
+    if (breakoutCanvas) {
+        breakoutCtx = breakoutCanvas.getContext('2d');
+
+        ballX = breakoutCanvas.width / 2;
+        ballY = breakoutCanvas.height - 30;
+
+        /*
+          Create anonymous function so actual function isn't
+          executed right away.
+          Otherwise setInterval() doesn't work.
+        */
+        setInterval(function () { drawBreakoutGame(); }, 10);
+    }
+}
+
+
+function drawBreakoutGame() {
+    breakoutCtx.clearRect(0, 0, breakoutCanvas.width, breakoutCanvas.height);
+
+    drawBall();
+}
+
+
+function drawBall() {
+    breakoutCtx.beginPath();
+    breakoutCtx.arc(ballX, ballY, 10, 0, Math.PI * 2);
+    breakoutCtx.fillStyle = '#FF0000';
+    breakoutCtx.fill();
+    breakoutCtx.closePath();
+
+    ballX += ballDX;
+    ballY += ballDY;
+}
