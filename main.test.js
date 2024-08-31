6be005a15ab6f2456a0a957cc71d2c8b3a51778a
CrazyChair69 <Christianschwanse@gmx.de>
2024-08-31 11:34:44 +0200

    Add Test Files Templates

---
+++ main.test.js
@@ -0,0 +1,5 @@
+// TODO: main.test.js => Add Unit Test debounce-func.
+// TODO: header.js => Add Unit Tests
+// TODO: language-switch.js => Add Unit Tests
+// TODO: table-of-contents.js => Add Unit Tests
+// TODO: theme-switch.js => Add Unit Tests
