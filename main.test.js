bfd7c8fefebb9d31a835452330695a544c383c6d
CrazyChair69 <Christianschwanse@gmx.de>
2024-08-31 17:13:56 +0200

    Add TODO Comments

--- main.test.js
+++ main.test.js
@@ -3,3 +3,5 @@
 // TODO: language-switch.js => Add Unit Tests
 // TODO: table-of-contents.js => Add Unit Tests
 // TODO: theme-switch.js => Add Unit Tests
+
+// TODO: Finish watching the video before tests are created (https://www.youtube.com/watch?v=x6NUZ8dc9Qg).
