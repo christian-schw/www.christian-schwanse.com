93b496a5a489b57f306c2c0c3f5d18d3502d7719
CrazyChair69 <Christianschwanse@gmx.de>
2024-09-01 14:02:20 +0200

    Change TODO List + Delete Test-JS-Files

--- components/table-of-contents/table-of-contents.test.js
+++
@@ -1,1 +0,0 @@
-// TODO: Implement Unit Tests
--- main.test.js
+++ main.test.js
@@ -1,10 +1,10 @@
-// TODO: main.test.js => Add Unit Test debounce-func.
-// TODO: header.js => Add Unit Tests
+// TODO: main.test.js => Add Unit Test debounce-func. 
+//   If no solution found to ReferenceError (only) in Unit Test
+//   then refactor debounce method to different file (maybe utilities-folder with utilities.js?)
+//   In this case, main.test.js is not necessary anymore.
 // TODO: language-switch.js => Add Unit Tests
-// TODO: table-of-contents.js => Add Unit Tests
 // TODO: theme-switch.js => Add Unit Tests
 
-// TODO: Finish watching the video before tests are created (https://www.youtube.com/watch?v=x6NUZ8dc9Qg).
 import { debounce } from './main';
 
 // TODO: Add describe () -> look up video
