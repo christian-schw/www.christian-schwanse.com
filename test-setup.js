0e7afdd259189377e55eb4d900015f11e5296374
CrazyChair69 <Christianschwanse@gmx.de>
2024-09-03 17:26:54 +0200

    Code Refactoring mockSessionStorage

--- test-setup.js
+++ test-setup.js
@@ -6,20 +6,18 @@ import { jest } from '@jest/globals';
 /*
   To test sessionStorage in Jest, you have to mock the sessionStorage
   in unit test to be able to call it.
+  Also jsdom is needed for window-Object.
 */
-/* const mockSessionStorage = {
+const mockSessionStorage = {
     mockGetItem: jest.fn(),
     mockSetItem: jest.fn(),
     mockRemoveItem: jest.fn()
-} */
-const mockGetItem = jest.fn();
-const mockSetItem = jest.fn();
-const mockRemoveItem = jest.fn();
+}
 
 Object.defineProperty(window, 'sessionStorage', {
     value: {
-        getItem: (...args) => mockGetItem(...args),
-        setItem: (...args) => mockSetItem(...args),
-        removeItem: (...args) => mockRemoveItem(...args)
+        getItem: (...args) => mockSessionStorage.mockGetItem(...args),
+        setItem: (...args) => mockSessionStorage.mockSetItem(...args),
+        removeItem: (...args) => mockSessionStorage.mockRemoveItem(...args)
     }
 });
