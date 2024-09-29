56e4f657fde6883e39fe14e9e5633f9425b22115
CrazyChair69 <Christianschwanse@gmx.de>
2024-09-29 16:39:55 +0200

    Add ESLint Config used by ESLint Team

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,25 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import eslintConfigESLint from "eslint-config-eslint";
+import eslintConfigESLintFormatting from "eslint-config-eslint/formatting";
 
 
 export default [
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   pluginJs.configs.recommended,
+  ...eslintConfigESLint,
+  eslintConfigESLintFormatting,
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    },
+
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn"
+    }
+  },
 ];
